1217d1922b63e0358bc0ac3d5ff15092d5c07782
Eric Cheng <eric310@connect.hku.hk>
2023-11-28 18:43:52 +0800

    Type hints, formatting cleanup
    Also simplifies PyInstaller workaround
    
    Signed-off-by: Eric Cheng <eric310@connect.hku.hk>

--- wordle.py
+++ wordle.py
@@ -1,17 +1,20 @@
 import random as rand
 
+
 def readfile(path):
     with open(path, "r") as f:
         words = f.readlines()
     return words
-    
+
+
 def pickword(words):
     ans = rand.choice(words)
     return ans.lower().strip("\n")
 
+
 def ask():
     error = True
-    while error == True:
+    while error:
         guess = input("Guess: ")
         if guess == "q":
             break
@@ -23,20 +26,21 @@ def ask():
             error = False
     return guess.lower()
 
-def scan(str):
+
+def scan(s: str):
     a = {}
-    for letter in str:
+    for letter in s:
         if letter not in a.keys():
-            a[letter] = str.count(letter)
+            a[letter] = s.count(letter)
     return a
 
-def play(ans, lives):
-    quit = False
+
+def play(ans, lives) -> bool:
+    _quit = False
     win = False
     counts = scan(ans)
-    while win == False and lives > 0:
-        print("")
-        print("Lives: ", lives)
+    while not win and lives > 0:
+        print("\nLives: ", lives)
         guess = ask()
         if guess == "q":
             break
@@ -44,7 +48,7 @@ def play(ans, lives):
             win = True
         else:
             reply = []
-            #green boxes and black boxes:
+            # green boxes and black boxes:
             for i in range(len(guess)):
                 if guess[i] == ans[i]:
                     reply.append("🟩")
@@ -53,7 +57,7 @@ def play(ans, lives):
                     reply.append("?")
                 else:
                     reply.append("⬛")
-            #yellow boxes + capable of handling duplicate words:
+            # yellow boxes + capable of handling duplicate words:
             for k in range(len(guess)):
                 if reply[k] == "?":
                     if counts[guess[i]] > 0:
@@ -61,38 +65,39 @@ def play(ans, lives):
                         counts[guess[i]] -= 1
                     else:
                         reply[k] = "⬛"
-            #print reply:
+            # print reply:
             for j in range(len(reply)):
                 print(reply[j], end="")
             lives -= 1
-    if win == True:
+    if win:
         print("You win!")
-    elif lives == 0: 
+    elif lives == 0:
         print("Out of lives!")
     else:
         print("Break from game")
-        quit = True
-    return quit
+        _quit = True
+    return _quit
+
 
 def playgame():
     print("Welcome to my own wordle")
     inp = input("(enter to play, type \'q\' to break, type \'*\' to show answer): ")
     if inp == "":
         debug_mode = False
-        quit = False
+        _quit = False
     elif inp == "*":
         debug_mode = True
-        quit = False
+        _quit = False
         print("enter cheating mode")
     else:
-        quit = True
-    while quit == False:
+        _quit = True
+    while not _quit:
         answer = pickword(readfile("words.txt"))
-        if debug_mode == True:
+        if debug_mode:
             print(answer)
-        quit = play(answer, 6)
+        _quit = play(answer, 6)
     print("Break from program")
+
 
 if __name__ == "__main__":
     playgame()
-
